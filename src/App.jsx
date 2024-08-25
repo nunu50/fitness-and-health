b634390b15c5f5138fba5313b0023d41f90cdabe
nuha samson <146358815+nunu50@users.noreply.github.com>
2024-08-25 08:31:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
      <HealthSection/>
      <FitnessSection/>
      <FoodsSection/>
-     
+     <script async src="https://fundingchoicesmessages.google.com/i/pub-3532739967438394?ers=1" nonce="e_9gMYbiIeIO-GfWraoFbQ"></script><script nonce="e_9gMYbiIeIO-GfWraoFbQ">(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();</script>
     </>
   )
 }
